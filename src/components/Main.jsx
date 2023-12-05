f7173fead3dec23644f7240767227fc5c39ae7bb
Makorii <marcelaromina.d@hotmail.com>
2023-12-04 22:01:27 -0300

    creo array de tareas junto al localStorage y agrego funcion para agregar una nueva junto con su componente

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import NewTask from './NewTask'
+import { Container } from '@chakra-ui/react'
+import { useState } from 'react'
 
 function Main() {
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || "")
+
+    const onSubmit = (task, e) => {
+      e.preventDefault()
+      const NewTask = {
+        nombre: task,
+        id: self.crypto.randomUUID(),
+        estado: false,
+      }
+      localStorage.setItem("tasks", JSON.stringify([...tasks, NewTask]))
+      setTasks([...tasks, NewTask])
+    }
   return (
-    <div>Main</div>
+    <Container maxW="container.sm">
+      <NewTask onSubmit={onSubmit} />
+    </Container>
   )
 }
 
