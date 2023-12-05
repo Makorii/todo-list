7819eb27aa556f0dd540ff8ac427a36f693f128d
Makorii <marcelaromina.d@hotmail.com>
2023-12-04 22:24:09 -0300

    agrego funciones onClose y OnStatus junto con sus componentes correspondientes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import NewTask from './NewTask'
-import { Container } from '@chakra-ui/react'
+import { Box, Center, Container } from '@chakra-ui/react'
 import { useState } from 'react'
+import ContainerTasks from './ContainerTasks'
 
 function Main() {
   const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || "")
@@ -16,9 +17,38 @@ function Main() {
       localStorage.setItem("tasks", JSON.stringify([...tasks, NewTask]))
       setTasks([...tasks, NewTask])
     }
+
+    const onClose = (id) => {
+      let filteredTasks = tasks.filter((task) => task.id !== id)
+      localStorage.setItem("tasks", JSON.stringify(filteredTasks))
+      setTasks(filteredTasks)
+    }
+  
+    const onStatus = (id) => {
+      const updatedTasks = tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            estado: !task.estado,
+          };
+        }
+        return task;
+      });
+    
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      setTasks(updatedTasks);
+    };
   return (
     <Container maxW="container.sm">
       <NewTask onSubmit={onSubmit} />
+      <Center>
+        <Box>
+          <ContainerTasks 
+            arrTask={tasks}
+            onClose={onClose}
+            onStatus={onStatus}/>
+        </Box>
+      </Center>
     </Container>
   )
 }
