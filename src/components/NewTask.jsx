476701b23611945e643933cf744791646755dce2
Makorii <marcelaromina.d@hotmail.com>
2023-12-06 10:26:34 -0300

    agrego funcion addTask para que al agregar una nueva tarea me limpie el input

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -6,10 +6,17 @@ import { Button} from '@chakra-ui/react'
 
 function NewTask({onSubmit}) {
   const [task, setTask] = useState("")
+
+  const addTask = (e) => {
+    e.preventDefault();
+    onSubmit(task, e);
+    setTask("");
+  };
+
   return (
     <Box>
       <Input placeholder='Ingresar nueva tarea' value={task} onChange={(e) => setTask(e.target.value)}/>
-      <Button colorScheme='purple' type='submit' onClick={(e) => onSubmit(task, e)} my={5}>Button</Button>
+      <Button colorScheme='purple' type='submit' onClick={(e) => addTask(e)} my={5}>Button</Button>
     </Box>
   )
 }
